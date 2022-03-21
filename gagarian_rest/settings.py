cb9de6dbef3ef4b53f1e0a9d503bbf84d89fe073
matt468779 <mengistusima@gmail.com>
2022-03-21 18:10:15 +0300

    settings edited

--- gagarian_rest/settings.py
+++ gagarian_rest/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_on_heroku
+
+DJANGO_SETTINGS_MODULE = "gagarian_rest.settings"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-20!v67d*h74+tw3a+36r06etwvf+8@tziaf_efo_=ncdj0*r3e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["gagarian.com", "www.gagarian.com"]
 
 
 # Application definition
@@ -183,4 +184,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #activate
-django_on_heroku.settings(locals())
+# django_on_heroku.settings(locals())
